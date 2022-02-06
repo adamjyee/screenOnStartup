ab644bfafe6d81508336d584bc0f798f76b58ce0
Adam Wright <adamjwright@rocketmail.com>
2022-02-06 15:09:34 +0000

    Explained digit library

--- digitLibrary.py
+++ digitLibrary.py
@@ -1,3 +1,7 @@
+# tiny library to return a digit in seven segment form
+# used https://roboindia.com/tutorials/raspberry-seven-segment
+# not sure why 1 is off, and 0 is on
+
 def digits(string digit):
     if digit == clr:
         return([1,1,1,1,1,1,1])
