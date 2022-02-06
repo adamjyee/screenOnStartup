1b4bf83bd8a57b8328f26ec1b4981f697545fcc4
Adam Wright <adamjwright@rocketmail.com>
2022-02-06 15:26:43 +0000

    Created new functions and changed lgpio's nickname
    Created setup pins function - which opens all the output pins
    Created pin list - outputs all pins which will be used
    Created clear display - used to clear and output to a display
    Changed lgpio to be called GPIO

--- getIPAddress.py
+++ getIPAddress.py
@@ -2,10 +2,10 @@
 # https://ubuntu.com/tutorials/gpio-on-raspberry-pi#3-basic-gpio-example
 # used https://roboindia.com/tutorials/raspberry-seven-segment/
 
-import os, socket, fcntl, struct, time, lgpio, sys, digitLibrary
+import os, socket, fcntl, struct, time, sys, digitLibrary
+import lgpio as GPIO
 
-import os, socket, fcntl, struct, time, lgpio
-
+GPIO.setmode(GPIO.BOARD)
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,27 +21,49 @@ def printIP():
     print("Wifi: ", get_ip_address('wlan0'))
     print("Ethernet: ", get_ip_address('eth0'))
 
+def setupPins():
+    #setup output pins
+    GPIO.setup(35, GPIO.OUT)      //GPIO19
+    GPIO.setup(12, GPIO.OUT)      //GPIO18
+    GPIO.setup(36, GPIO.OUT)      //GPIO16
+    GPIO.setup(33, GPIO.OUT)      //GPIO13
+    GPIO.setup(32, GPIO.OUT)      //GPIO12
+    GPIO.setup(38, GPIO.OUT)      //GPIO20
+    GPIO.setup(40, GPIO.OUT)      //GPIO21
+
+def pinList():
+    return([35,12,36,33,32,38,40])
+
+# clears display
+def clearWriteDisplay(digit):
+    gpin = pinList()
+    for x in range (0,7):
+        GPIO.output(gpin[x], digitLibrary.digits("clr")[x])
+        for i in range (0,7):
+            GPIO.output(gpin[x], digitLibrary.digits(digit)[x])
+
+
 def GPIO():
     #describes pins to output to
     pin = 0
 
-    h = lgpio.gpiochip_open(0)
-    lgpio.gpio_claim_output(h, pin)
+    h = GPIO.gpiochip_open(0)
+    GPIO.gpio_claim_output(h, pin)
 
     try:
         while True:
             # turn on gpio pin
             # the 1 is "on"
-            lgpio.gpio_write(h, pin, 1)
+            GPIO.gpio_write(h, pin, 1)
             time.sleep(1)
 
             # turn the gpio pin off
             # the 0 is "off"
-            lgpio.gpio_write(h, pin, 0)
+            GPIO.gpio_write(h, pin, 0)
             time.sleep(1)
 
     #runs until interrupted by keyboard press
     except KeyboardInterrupt:
-        lgpio.gpio_write(h, pin, 0)
-        lgpio.gpiochip_close(h)
+        GPIO.gpio_write(h, pin, 0)
+        GPIO.gpiochip_close(h)
 
